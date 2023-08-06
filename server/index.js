e058aec2487ed9740b250a345287b4e6c9113b2b
Arindam Chowdhury <arindamchy00@gmail.com>
2023-08-06 21:08:33 +0530

    change

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ app.use(cors())
 app.use(express.json())
 
 app.get("/", (req, res) => {
-	res.send("Hi there")
+	res.send("Hi there!!")
 })
 app.use("/admin", adminRouter)
 app.use("/user", userRouter)
